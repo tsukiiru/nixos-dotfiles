4cfb9ab201685c3891a76c736bd320862c7fe005
tsuki <108170925+tsukiiru@users.noreply.github.com>
2026-07-04 16:56:10 +0700

    sourcing

--- home-programs/main.nix
+++ home-programs/main.nix
@@ -1,5 +1,5 @@
 { }: {
-  zen-browser = import ./zen-browser.nix;
-  nixcord = import ./nixcord.nix;
-  fish = import ./fish.nix;
+  zen-browser = import ./zen-browser.nix { };
+  nixcord = import ./nixcord.nix { };
+  fish = import ./fish.nix { };
 }
