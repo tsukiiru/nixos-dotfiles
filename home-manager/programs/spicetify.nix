88ef9e68a215b3616e87421a69a271bec753f4a0
tsuki <108170925+tsukiiru@users.noreply.github.com>
2026-08-06 13:01:15 +0700

    it's working!! :D

--- home-manager/programs/spicetify.nix
+++ home-manager/programs/spicetify.nix
@@ -1,7 +1,38 @@
 { inputs, pkgs, ... }:
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-  themePkg = spicePkgs.themes.text;
+  themePkg = spicePkgs.themes.text // {
+    additionalCss = ''
+      /* user settings */
+      :root {
+          --font-family: "Annotation Mono", monospace;
+          --font-size: 14px;
+          --font-weight: 400; /* 200 : 900 */
+          --line-height: 1.2;
+
+          --font-size-lyrics: 14px; /* 1.5em (default) */
+
+          --font-family-header: "asciid";
+          --font-size-multiplier-header: 4;
+
+          --display-card-image: block; /* none | block */
+          --display-coverart-image: none; /* none | block */
+          --display-header-image: none; /* none | block */
+          --display-sidebar-image: block; /* none | block */
+          --display-tracklist-image: none; /* none | block */
+          --display-spicetify-banner-ascii: block; /* none | block */
+          --display-music-banner-ascii: none; /* none | block */
+
+          --border-radius: 0px;
+          --border-width: 1px;
+          --border-style: solid; /* dotted | dashed | solid | double | groove | ridge | inset | outset */
+          --border-transition: 0.2s ease; /* 'none' to disable  */
+
+          --global-nav-margin-top: 0px; /* set to '0px' if you disabled window controls */
+      }
+    '';
+  };
+
 in
 {
   enable = true;
@@ -9,7 +40,6 @@ in
   enabledExtensions = with spicePkgs.extensions; [
     adblock
     shuffle
-    oldSidebar
     betterGenres
   ];
   enabledCustomApps = with spicePkgs.apps; [
@@ -17,35 +47,6 @@ in
   ];
 
   alwaysEnableDevTools = true;
-  themePkgs.additionalCss = ''
-    /* user settings */
-    :root {
-        --font-family: "Annotation Mono", monospace;
-        --font-size: 14px;
-        --font-weight: 400; /* 200 : 900 */
-        --line-height: 1.2;
-
-        --font-size-lyrics: 14px; /* 1.5em (default) */
-
-        --font-family-header: "asciid";
-        --font-size-multiplier-header: 4;
-
-        --display-card-image: block; /* none | block */
-        --display-coverart-image: none; /* none | block */
-        --display-header-image: none; /* none | block */
-        --display-sidebar-image: block; /* none | block */
-        --display-tracklist-image: none; /* none | block */
-        --display-spicetify-banner-ascii: block; /* none | block */
-        --display-music-banner-ascii: none; /* none | block */
-
-        --border-radius: 0px;
-        --border-width: 1px;
-        --border-style: solid; /* dotted | dashed | solid | double | groove | ridge | inset | outset */
-        --border-transition: 0.2s ease; /* 'none' to disable  */
-
-        --global-nav-margin-top: 0px; /* set to '0px' if you disabled window controls */
-    }
-  '';
   theme = themePkg;
   colorScheme = "RosePineDawn";
 }
