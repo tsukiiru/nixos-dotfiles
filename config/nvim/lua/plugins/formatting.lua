18a95714f350232f50c89b30992990e0de04ab66
tsukiiru <108170925+tsukiiru@users.noreply.github.com>
2026-08-22 12:12:09 +0700

    (nvim) trimming

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -1,34 +1,48 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPost", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettier" },
-				rust = { "rust-analyzer" },
-				html = { "prettier" },
-				css = { "prettier" },
-				python = { "black" },
-				kdl = { "kdlfmt" },
-				nix = { "nixfmt" },
-				json = { "prettier " },
-			},
-			format_on_save = {
-				timeout_ms = 1000,
-				lsp_fallback = true,
-				async = false,
-			},
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
+			conform.setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettier" },
+					rust = { "rustfmt" },
+					html = { "prettier" },
+					css = { "prettier" },
+					python = { "black" },
+					kdl = { "kdlfmt" },
+					nix = { "nixfmt" },
+					json = { "prettier " },
+				},
+				format_on_save = {
+					timeout_ms = 1000,
+					lsp_fallback = true,
+					async = false,
+				},
 			})
-		end, { desc = "format file or range (visual mode)" })
-	end,
+
+			vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end, { desc = "format file or range (visual mode)" })
+		end,
+	},
+	{
+		"cappyzawa/trim.nvim",
+		config = function()
+			require("trim").setup({
+				trim_on_write = false,
+				highlight = true,
+				patterns = {
+					[[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
+				},
+			})
+		end,
+	},
 }
